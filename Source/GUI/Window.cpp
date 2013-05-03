11a6735a435b58d39d021387cf980ef12826183f
BTH <bth@bth.se>
2013-05-03 19:54:52 +0200

    Minor change

--- Source/GUI/Window.cpp
+++ Source/GUI/Window.cpp
@@ -35,7 +35,7 @@ Window::Window()
 	m_manager_commands = new Manager_Commands();
 	m_manager_commands->init();
 
-	//m_manager_docks->resetLayout();
+	m_manager_docks->resetLayout();
 
 	m_refreshTimer = new QTimer();
 
